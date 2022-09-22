d4a4c0f497a266770c51c74f21a1673e1f01f2d6
Safei Ashraf <safei.ashraf@gmail.com>
2022-09-22 12:15:49 +0200

    feat: fetch static resources

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+//to be customized for frontend url
+const cors = require("cors");
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +25,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -24,7 +37,7 @@ app.use("/users", usersRouter);
 
 //routing
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send({ msg: "Hello World!" });
 });
 
 app.post("/", (req, res) => {
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,5 @@
 console.log("attached script (^_-)");
+
+fetch("http://localhost:3000/images/meme.jpg").then((data) =>
+	console.log(data)
+);
